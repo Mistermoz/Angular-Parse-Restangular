93193f0f3fd4772af92860c64147ce926a452284
claudio.donoso <claudio.donoso@STGUMW-NMX0009.local>
2015-09-25 14:06:49 -0300

    CRUD BRands

--- app/controllers/BrandsController.js
+++ app/controllers/BrandsController.js
@@ -30,61 +30,24 @@
     }
 
     if($location.path() == '/brands/add') {
-      that.name = '';
-      that.description = '';
+      that.brandAdd = {};
       that.showView = true;
       $rootScope.isLoading = false;
 
-      that.add = function () {
+      that.add = function (isValid) {
         var that = this, fd = '';
         $rootScope.isLoading = true;
 
-        var img =  $scope.myCroppedImage.replace(/^data:image\/(png|jpeg);base64,/, '');
-
-        fd = {__ContentType : "image/jpeg", base64 : img};
-
-        Restangular.one('files/pic.jpg').customPOST(fd, undefined, undefined, {'Content-Type': 'text/plain'}).then(function (response) {
-          var newBrand = {"name": that.name, "description": that.description, image: {"name": response.url, "__type": "File"}, createdBy: $rootScope.name};
-          BrandModel.post(newBrand).then(function (brand) {
-            $location.path('/brands');
-            $rootScope.isLoading = false;
-          }, function (error) {
-            console.log(error);
-            $rootScope.isLoading = false;
-          });
-        }, function (error) {
-          console.log(error);
-          $rootScope.isLoading = false;
-        });
-      };
-    }
-
-    if($location.path().indexOf("/brands/view") > -1 ) {
-      that.view = {};
-      that.editImage = false;
-
-      BrandModel.one($routeParams.objId).get().then(function (brandObejct) {
-        that.view = brandObejct;
-        that.showView = true;
-        $rootScope.isLoading = false;
-      }, function (error) {
-        console.log(error);
-      });
-
-      that.edit = function () {
-        $rootScope.isLoading = true;
-
-        if(that.editImage && $scope.myCroppedImage !== '') {
-          var img =  $scope.myCroppedImage.replace(/^data:image\/(png|jpeg);base64,/, ''), fd = '';
+        if(isValid) {
+          var img =  $scope.myCroppedImage.replace(/^data:image\/(png|jpeg);base64,/, '');
 
           fd = {__ContentType : "image/jpeg", base64 : img};
 
           Restangular.one('files/pic.jpg').customPOST(fd, undefined, undefined, {'Content-Type': 'text/plain'}).then(function (response) {
-            that.view.image = {"name": response.url, "__type": "File"};
-            that.view.put().then(function (rsp) {
-              $scope.myCroppedImage = '';
-              $scope.myImage = '';
-              that.editImage = false;
+            that.brandAdd.image = {"name": response.url, "__type": "File"};
+            that.brandAdd.createdBy = $rootScope.name;
+            BrandModel.post(that.brandAdd).then(function (brand) {
+              $location.path('/brands');
               $rootScope.isLoading = false;
             }, function (error) {
               console.log(error);
@@ -95,13 +58,59 @@
             $rootScope.isLoading = false;
           });
         }else {
-            that.view.put().then(function (rsp) {
-              console.log(rsp);
+          $rootScope.isLoading = false;
+        }
+      };
+    }
+
+    if($location.path().indexOf("/brands/view") > -1 ) {
+      that.brandView = {};
+      that.editImage = false;
+
+      BrandModel.one($routeParams.objId).get().then(function (brandObejct) {
+        that.brandView = brandObejct;
+        that.showView = true;
+        $rootScope.isLoading = false;
+      }, function (error) {
+        console.log(error);
+      });
+
+      that.edit = function (isValid) {
+        $rootScope.isLoading = true;
+
+        if(isValid) {
+          if(that.editImage && $scope.myCroppedImage !== '') {
+            var img =  $scope.myCroppedImage.replace(/^data:image\/(png|jpeg);base64,/, ''), fd = '';
+
+            fd = {__ContentType : "image/jpeg", base64 : img};
+
+            Restangular.one('files/pic.jpg').customPOST(fd, undefined, undefined, {'Content-Type': 'text/plain'}).then(function (response) {
+              that.brandView.image = {"name": response.url, "__type": "File"};
+              that.brandView.put().then(function (rsp) {
+                $scope.myCroppedImage = '';
+                $scope.myImage = '';
+                that.editImage = false;
+                $rootScope.isLoading = false;
+                SweetAlert.swal("Edición Satisfactoria");
+              }, function (error) {
+                console.log(error);
+                $rootScope.isLoading = false;
+              });
+            }, function (error) {
+              console.log(error);
               $rootScope.isLoading = false;
-          }, function (error) {
-            $rootScope.isLoading = false;
-            console.log(error);
-          });
+            });
+          }else {
+              that.brandView.put().then(function (rsp) {
+                SweetAlert.swal("Edición Satisfactoria");
+                $rootScope.isLoading = false;
+            }, function (error) {
+              $rootScope.isLoading = false;
+              console.log(error);
+            });
+          }
+        }else {
+          $rootScope.isLoading = false;
         }
       };
     }
@@ -116,8 +125,8 @@
          showCancelButton: true,
          confirmButtonColor: "#DD6B55",
          confirmButtonText: "Si, eliminar!",
-         closeOnConfirm: false}, 
-      function(response){ 
+         closeOnConfirm: false
+       }, function(response){ 
         if(response) {
           BrandModel.one(obj).remove().then(function (response) {
             console.log(response);
